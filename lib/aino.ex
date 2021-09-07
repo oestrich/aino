fc960aeb2ff5c39200c98046b5c312d0a04e1580
Eric Oestrich <eric@oestrich.org>
2021-09-06 23:25:50 -0400

    Add Aino.View to help compile templates
    And generate render functions that don't whine when you don't have
    assigns in the template (yet)

--- lib/aino.ex
+++ lib/aino.ex
@@ -538,3 +538,45 @@ defmodule Aino.Request do
     %__MODULE__{unquote_splicing(pairs)}
   end
 end
+
+defmodule Aino.View do
+  @moduledoc """
+  Compile templates into modules with a render function
+
+  ```elixir
+  compile [
+    "lib/app/templates/folder/index.html.eex"
+  ]
+  ```
+
+  Generates the following render functions:
+
+  ```elixir
+  def render("index.html", assigns) do
+    # compiled index.html.eex
+  end
+  ```
+  """
+
+  defmacro compile(files) when is_list(files) do
+    Enum.map(files, fn file ->
+      Aino.View.compile_template(file)
+    end)
+  end
+
+  def compile_template(file) do
+    filename = Path.basename(file)
+    filename = String.replace(filename, ~r/\.eex$/, "")
+
+    quote bind_quoted: [file: file, filename: filename] do
+      require EEx
+
+      compiled = EEx.compile_file(file, [])
+
+      def render(unquote(filename), var!(assigns)) do
+        _ = var!(assigns)
+        unquote(compiled)
+      end
+    end
+  end
+end
