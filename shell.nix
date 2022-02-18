2b7b98b02e8fdd9241191a6fabf3504fe4509268
Eric Oestrich <eric@oestrich.org>
2022-02-17 22:14:22 -0500

    Setup envrc & shell.nix for local development

---
+++ shell.nix
@@ -0,0 +1,36 @@
+# change the pkgs import to a tag when there is a 22.XX version
+# at the moment we need a specific SHA to be able to use m1 chromedriver
+{
+  lib ? import <lib> {},
+  pkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs/archive/a40082270194f2068e6cfb4d26d53d511fc8c34b.zip) {}
+}:
+
+let
+
+  # define packages to install with special handling for OSX
+  basePackages = [
+    pkgs.gnumake
+    pkgs.gcc
+    pkgs.readline
+    pkgs.zlib
+    pkgs.libxml2
+    pkgs.libiconv
+    pkgs.openssl
+    pkgs.curl
+    pkgs.git
+
+    pkgs.erlangR24
+    pkgs.beam.packages.erlangR24.elixir_1_12
+  ];
+
+  inputs = basePackages
+    ++ [ pkgs.bashInteractive ]
+    ++ pkgs.lib.optionals pkgs.stdenv.isLinux [ pkgs.inotify-tools ]
+    ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs.darwin.apple_sdk.frameworks; [
+        CoreFoundation
+        CoreServices
+      ]);
+
+in pkgs.mkShell {
+  buildInputs = inputs;
+}
