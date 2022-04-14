ad85dfb79da8715835d43c96a1289e42c217dec3
Eric Oestrich <eric@oestrich.org>
2022-04-14 09:11:05 -0400

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Aino.MixProject do
   def project do
     [
       app: :aino,
-      version: "0.3.1",
+      version: "0.4.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
