3a522fef23388b4daea9e299e32904fed28e9e51
Eric Oestrich <eric@oestrich.org>
2022-08-20 14:30:10 -0400

    Add a mix task to generate a manifest

---
+++ lib/mix/tasks/aino.assets.ex
@@ -0,0 +1,27 @@
+defmodule Mix.Tasks.Aino.Assets do
+  @moduledoc """
+  Generate a manifest file for assets
+  """
+
+  use Mix.Task
+
+  @shortdoc "Generate an asset manifest file"
+
+  @impl true
+  def run(args) do
+    # Load the main application so the `otp_app` priv folder exists 
+    Mix.Task.run("app.config")
+
+    case Enum.count(args) == 1 do
+      true ->
+        [otp_app] = args
+        otp_app = String.to_atom(otp_app)
+        Aino.Assets.generate_manifest(otp_app)
+
+        IO.puts "Manifest generated"
+
+      false ->
+        IO.puts "You must include the OTP app to generate a manifest file"
+    end
+  end
+end
